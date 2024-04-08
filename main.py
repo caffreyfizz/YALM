a627586fd635a29f870ebe30215c720e56a57cf7
Никита <107890115+caffreyfizz@users.noreply.github.com>
2024-04-08 23:32:47 +0300

    Задача Какой район?

--- main.py
+++ main.py
@@ -21,23 +21,19 @@ if not response:
 # Преобразуем ответ в json-объект
 json_response = response.json()
 # Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"][
-    'metaDataProperty']['GeocoderResponseMetaData']['boundedBy']['Envelope']
 
 toponym_coodrinates = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
-delta1 = str(abs(float(toponym['lowerCorner'].split()[0]) - float(toponym['lowerCorner'].split()[1])))
-delta2 = str(abs(float(toponym['upperCorner'].split()[0]) - float(toponym['upperCorner'].split()[1])))
+params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": f"{toponym_longitude},{toponym_lattitude}",
+    "format": "json"}
 
-map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": get_spn(toponym),
-    "l": "map"
-}
-
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
-
-Image.open(BytesIO(
-    response.content)).show()
+adress = f"https://geocode-maps.yandex.ru/1.x"
+if not adress:
+    pass
+response = requests.get(adress, params=params).json()
+address_name = response["response"]["GeoObjectCollection"]["featureMember"][2][
+'GeoObject']['metaDataProperty']['GeocoderMetaData']["Address"]['Components'][4]["name"]
+print(address_name)
