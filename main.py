e099ac8407acda8c562373739e4e0b15dcd24d13
Никита <107890115+caffreyfizz@users.noreply.github.com>
2024-04-09 22:29:37 +0300

    Задача Пейзажи Марса

--- main.py
+++ main.py
@@ -3,29 +3,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
  
  
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def greeting(nickname, level, rating):
-    return f"""<!doctype html>
+@app.route('/carousel')
+def greeting():
+    return """<!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <title>Результаты</title>
+    <title>Пейзажи Марса</title>
 </head>
 
 <body>
-    <h1>Результаты отбора</h1>
-    <h2>Претендента на участие в миссии {nickname}:</h2>
-    <div class="alert alert-success" role="alert">
-        Поздравляем! Ваш рейтинг после {level} этапа отбора
-    </div>
-    <div class="alert alert-light" role="alert">
-      Составляет {rating}!
-    </div>
-    <div class="alert alert-warning" role="alert">
-        Жедаем удачи!
+    <h1 style='text-align:center'>👽Не фейк! На Марсе нашли инопланетян!👽</h1>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="/static/img/image1.png" alt="Первый слайд">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="/static/img/image2.png" alt="Второй слайд">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="/static/img/image3.png" alt="Третий слайд">
+        </div>
+      </div>
     </div>
 </body>
 
