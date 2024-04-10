2a6d68ee535bc054554883e74c693520d5d03935
Никита <107890115+caffreyfizz@users.noreply.github.com>
2024-04-11 01:28:43 +0300

    Задача По каютам!

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/list_prof/<type_list>')
-def index(type_list):
-    list_of_prof = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
-                   'инженер по терраформированию', 'климатолог',
-                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
-                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
-                   'штурман', 'пилот дронов']
-    return render_template('training.html', type_list=type_list, profs=list_of_prof)
+@app.route('/distribution')
+def index():
+    peoples = [
+            'Ридли Скотт',
+            "Энди Уир",
+            "Марк Уотни",
+            "Венката Капур",
+            "Тедди Сандерс",
+            "Шон Бин"
+        ]
+    return render_template('training.html', profs=peoples)
 
 
 if __name__ == '__main__':
